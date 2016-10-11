6a13da776a4b754f23fa22e9e08fa4f95f2d4425
Zoyisile <kubekasimon@gmail.com>
2016-10-11 15:07:05 +0200

    Rejected commit: Default (2)

--- app/src/main/java/com/example/admin/quizchallenge/Main2Activity.java
+++ app/src/main/java/com/example/admin/quizchallenge/Main2Activity.java
@@ -1,11 +1,14 @@
 package com.example.admin.quizchallenge;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 
 public class Main2Activity extends AppCompatActivity  {
     Button philosopohy, religion, mathematics;
@@ -40,5 +43,35 @@ public class Main2Activity extends AppCompatActivity  {
         Intent objIntent = new Intent(Main2Activity.this, Main4Activity.class);
         startActivity(objIntent);
 
+    }
+
+  // Exit the game to the main activity.
+    public  void exitButton (View view){
+
+        onBackPressed ();
+    }
+
+@Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Main2Activity.this);
+        builder.setCancelable(true);
+        builder.setMessage("Do you want to Exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                //if user pressed "yes", then he is allowed to exit from application
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                //if user select "No", just cancel this dialog and continue with app
+                dialog.cancel();
+
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
